fea14595412a528fd3a3247f69fb793bf94b71b0
kseen <korenevdn@gmail.com>
2023-08-06 20:36:21 +0300

    Portals v0.1

--- Assets/Scripts/Portal.cs
+++ Assets/Scripts/Portal.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace Labyrinth
@@ -5,35 +7,91 @@ namespace Labyrinth
     public class Portal : MonoBehaviour
     {
         private GameObject portal;
-        [SerializeField] private GameObject otherPortal;
+        // 4 portals in total for different dimensions
+        [SerializeField] private GameObject[] otherPortals = new GameObject[4];
+
+
 
         private void Start()
         {
             portal = gameObject;
+
+            // force disable mesh renderer of portal
+            portal.GetComponent<MeshRenderer>().enabled = false;
         }
 
 
         private void OnTriggerEnter(Collider other)
         {
-            // change only x and z coordinates
-            Vector3 tmp = otherPortal.transform.position;
-            tmp.y = other.transform.position.y;
-            other.transform.position = tmp;
+            short currentDimension = other.GetComponent<Player>().Dimension;
+            // check if player is in any dimension
+            if (currentDimension >= 0)
+            {
+                // calculate height of player
+                float height = other.GetComponent<Collider>().bounds.size.y;
 
-            // change rotation of player to match rotation of portal he is teleporting to, but only on y axis
-            other.transform.rotation = Quaternion.Euler(0, otherPortal.transform.rotation.eulerAngles.y, 0);
+                // change only x and z coordinates
+                Vector3 tmp = otherPortals[currentDimension].transform.position;
+                tmp.y += height - 0.2f; // Note (Denis): 0.2f is a small offset 
+                                        // to avoid jump after the portal
+                other.transform.position = tmp;
 
-            // rotate player by 180 on y axis
-            other.transform.Rotate(0, 180, 0);
+                // change rotation of player to match rotation of portal he is 
+                // teleporting to, but only on y axis
+                other.transform.rotation = Quaternion.Euler(0,
+                otherPortals[currentDimension].transform
+                .rotation.eulerAngles.y, 0);
 
-            // player should exit trigger to avoid teleporting back and forth
-            otherPortal.GetComponent<Collider>().enabled = false;
-            Invoke(nameof(EnablePortal), 1f);
+                // rotate player by 180 on y axis (bcoz portals facing in the 
+                // direction of theit input)
+                other.transform.Rotate(0, 180, 0);
+
+                // player should exit trigger to avoid teleporting back and 
+                // forth
+                DisablePortal(currentDimension);
+
+                // wait until player is out of trigger
+                while (other.GetComponent<Collider>().bounds.Intersects(
+                otherPortals[currentDimension].GetComponent<Collider>().bounds))
+                {
+                    // do nothing
+                }
+
+                // wait a bit before enabling portal to avoid teleporting back 
+                // and forth, do not use Inovke() because it is not working
+                // properly (also coroutine is just better ^-^ )
+                StartCoroutine(EnablePortalAfterDelay(currentDimension));
+            }
         }
 
-        private void EnablePortal()
+
+        private void DisablePortal(int index)
         {
-            otherPortal.GetComponent<Collider>().enabled = true;
+            otherPortals[index].GetComponent<Collider>().enabled = false;
+        }
+        private void EnablePortal(int index)
+        {
+            otherPortals[index].GetComponent<Collider>().enabled = true;
+        }
+
+        private IEnumerator<WaitForSeconds> EnablePortalAfterDelay(int index)
+        {
+            yield return new WaitForSeconds(1);
+            EnablePortal(index);
+        }
+
+        // if pressed 'ctrl+shift+alt+T' switcj mesh renderer of portals for
+        // debug purposes
+        private void Update()
+        {
+            if (Input.GetKey(KeyCode.LeftControl) &&
+                Input.GetKey(KeyCode.LeftShift) &&
+                Input.GetKey(KeyCode.LeftAlt) &&
+                Input.GetKeyDown(KeyCode.T))
+            {
+                portal.GetComponent<MeshRenderer>().enabled =
+                !portal.GetComponent<MeshRenderer>().enabled;
+            }
         }
 
     }
