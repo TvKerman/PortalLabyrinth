19359fc11b8dfe48eb634d401cbc1aae979b9d8c
kseen <korenevdn@gmail.com>
2023-08-06 16:58:18 +0300

    Conflict resolve in merge

--- Assets/Scripts/Node.cs
+++ Assets/Scripts/Node.cs
@@ -16,6 +16,7 @@ namespace Labyrinth
         private List<Int64> neighbors;
         private Int16 exitCount;
 <<<<<<< HEAD
+<<<<<<< HEAD
         private Int16 dimension;
 
         private Vector3 coordinates;
@@ -25,12 +26,16 @@ namespace Labyrinth
 =======
         private Int16 dimesion;
 >>>>>>> parent of 89fab3a (Merge remote-tracking branch 'remotes/origin/Roman')
+=======
+        private Int16 dimesion;
+>>>>>>> parent of 89fab3a (Merge remote-tracking branch 'remotes/origin/Roman')
         public Node(Int64 id, Int16 dimension)
         {
             this.id = id;
             this.dimesion = dimension;
             this.neighbors = new List<Int64>();
             this.exitCount = 0;
+<<<<<<< HEAD
         }
 
         public Node(Int64 id, Int16 dimension, NodeType type) : this(id, dimension) 
@@ -41,6 +46,8 @@ namespace Labyrinth
         public Node(Int64 id, Int16 dimension, NodeType type, Int16 exits) : this(id, dimension, type) 
         {
             this.exitCount = exits;
+=======
+>>>>>>> parent of 89fab3a (Merge remote-tracking branch 'remotes/origin/Roman')
         }
 
         public NodeType Type
