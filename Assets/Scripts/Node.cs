990ce4f4efcd40d0a54ecd186c77b64c49f27936
kseen <korenevdn@gmail.com>
2023-08-06 17:03:28 +0300

    Node cs conflict cleanup

--- Assets/Scripts/Node.cs
+++ Assets/Scripts/Node.cs
@@ -15,27 +15,19 @@ namespace Labyrinth
         private Int64 id;
         private List<Int64> neighbors;
         private Int16 exitCount;
-<<<<<<< HEAD
-<<<<<<< HEAD
         private Int16 dimension;
 
         private Vector3 coordinates;
 
   
 
-=======
         private Int16 dimesion;
->>>>>>> parent of 89fab3a (Merge remote-tracking branch 'remotes/origin/Roman')
-=======
-        private Int16 dimesion;
->>>>>>> parent of 89fab3a (Merge remote-tracking branch 'remotes/origin/Roman')
         public Node(Int64 id, Int16 dimension)
         {
             this.id = id;
             this.dimesion = dimension;
             this.neighbors = new List<Int64>();
             this.exitCount = 0;
-<<<<<<< HEAD
         }
 
         public Node(Int64 id, Int16 dimension, NodeType type) : this(id, dimension) 
@@ -46,8 +38,6 @@ namespace Labyrinth
         public Node(Int64 id, Int16 dimension, NodeType type, Int16 exits) : this(id, dimension, type) 
         {
             this.exitCount = exits;
-=======
->>>>>>> parent of 89fab3a (Merge remote-tracking branch 'remotes/origin/Roman')
         }
 
         public NodeType Type
